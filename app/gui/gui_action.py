db82abcefa46186239d8c1532ca2652bc1a1b521
wisnialeszno89 <wisnialeszno89@gmail.com>
2026-08-12 12:16:07 +0200

    refactor: add semantic intent to GuiAction

--- app/gui/gui_action.py
+++ app/gui/gui_action.py
@@ -5,6 +5,10 @@ from app.gui.enums.gui_tool import (
     GuiTool,
 )
 
+from app.gui.enums.gui_intent import (
+    GuiIntent,
+)
+
 from app.construction.models.field import (
     Field,
 )
@@ -18,6 +22,12 @@ class GuiAction:
     #
 
     tool: GuiTool
+
+    #
+    # Semantic intent of the GUI operation.
+    #
+
+    intent: GuiIntent = GuiIntent.EDIT
 
     #
     # Component selection.
